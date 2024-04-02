43720b3c6ea2d47e158007c1c6e38c80fa788a6a
Emilia <s27827@pjwstk.edu.pl>
2024-04-02 21:50:02 +0200

    update

---
+++ zadanie/LegacyApp/UserService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        private readonly ClientRepository _clientRepository;
+
+        public UserService()
+        {
+            _clientRepository = new ClientRepository();
+        }
+
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || !IsValidEmail(email) || !IsAdult(dateOfBirth))
+            {
+                return false;
+            }
+
+            var client = _clientRepository.GetById(clientId);
+            if (client == null)
+            {
+                return false;
+            }
+
+            var user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            if (client.Type == "VeryImportantClient")
+            {
+                user.HasCreditLimit = false;
+            }
+            else
+            {
+                using (var userCreditService = new UserCreditService())
+                {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    if (client.Type == "ImportantClient")
+                    {
+                        creditLimit *= 2;
+                    }
+                    user.CreditLimit = creditLimit;
+                    user.HasCreditLimit = true;
+                }
+            }
+
+            if (user.HasCreditLimit && user.CreditLimit < 500)
+            {
+                return false;
+            }
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            return email.Contains("@") && email.Contains(".");
+        }
+
+        private bool IsAdult(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+            return age >= 21;
+        }
+    }
+}
